129c2a2c63f903c9d5d477587d68160fdf318a0c
thekoon0456 <thekoon0456@gmail.com>
2023-01-17 17:55:39 +0900

    [Add] TabView
    탭뷰 추가했습니다.

---
+++ BootCamping/BootCamping/MyCampingDiary/MyCampingDiaryView.swift
@@ -0,0 +1,20 @@
+//
+//  MyCampingDiary.swift
+//  BootCamping
+//
+//  Created by Deokhun KIM on 2023/01/17.
+//
+
+import SwiftUI
+
+struct MyCampingDiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyCampingDiaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyCampingDiaryView()
+    }
+}

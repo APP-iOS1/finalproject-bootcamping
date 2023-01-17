43bf698d940ba397fce57835d76fa924a1cb47f2
thekoon0456 <thekoon0456@gmail.com>
2023-01-17 17:30:04 +0900

    [Add] BootCampingProject
    BootCampingProject 시작 파일입니다.

---
+++ BootCamping/BootCamping/UIScreen+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen+Extension.swift
+//  BootCamping
+//
+//  Created by Deokhun KIM on 2023/01/17.
+//
+
+import SwiftUI
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
---
+++ BootCamping/BootCamping/ViewModifiers.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModifiers.swift
+//  BootCamping
+//
+//  Created by Deokhun KIM on 2023/01/17.
+//
+
+import SwiftUI

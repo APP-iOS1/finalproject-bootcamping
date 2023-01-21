8f8b3c7c5fc4b623936ca7f603bdb8e78fd698fb
erun94510 <erun9414@icloud.com>
2023-01-21 21:06:07 +0900

    [Refactor] 로그인페이지 버튼 분리

--- BootCamping/BootCamping/Views/Auth/LoginView.swift
+++ BootCamping/BootCamping/Views/Auth/LoginView.swift
@@ -34,59 +34,17 @@ struct LoginView: View {
                             .disableAutocorrection(true)
                             .autocapitalization(.none)
                             .padding()
-                     
+                        
                     }
-                NavigationLink {
-                    if isLogin {
-                        AuthSignUpView(userEmail: userEmail)
-                    } else {
-                        LoginPasswordView(userEmail: userEmail, isSignIn: $isSignIn)
-                    }
-                } label: {
-                    Text("계속")
-                        .modifier(GreenButtonModifier())
-                }
+                signInAndSignUpButton
                 
-                Divider()
-                    .padding(.horizontal, UIScreen.screenWidth * 0.05)
-                    .padding(.vertical, 10)
+                Divider().padding(.horizontal, UIScreen.screenWidth * 0.05).padding(.vertical, 10)
                 
-                Button {
-                    kakaoAuthStore.handleKakaoLogin()
-                    }
-                    
-                 label: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.gray)
-                        .frame(width: UIScreen.screenWidth * 0.8, height: 44)
-                        .overlay {
-                            Text("카카오로 로그인하기")
-                        }
-                }
-                Button {
-                    authStore.googleSignIn()
-                } label: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.gray)
-                        .frame(width: UIScreen.screenWidth * 0.8, height: 44)
-                        .overlay {
-                            Text("Google로 로그인하기")
-                        }
-                }
-                Button {
-                    
-                } label: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(.black)
-                        .frame(width: UIScreen.screenWidth * 0.8, height: 44)
-                        .overlay {
-                            HStack {
-                                Image(systemName: "applelogo")
-                                Text("Apple로 로그인하기")
-                            }
-                                .foregroundColor(.white)
-                        }
-                }
+                kakaoLoginButton
+                
+                googleLoginButton
+                
+                appleLoginButton
                 Spacer()
             }
             .foregroundColor(Color("BCBlack"))
@@ -98,8 +56,69 @@ struct LoginView: View {
             }
         }
     }
+    
+    // 로그인 혹은 회원가입 버튼
+    /// 이메일 체크 후 중복 시 로그인 페이지로
+    /// 이메일 체크 후 중복 없을 시 회원가입 페이지로
+    var signInAndSignUpButton: some View {
+        NavigationLink {
+            if isLogin {
+                AuthSignUpView(userEmail: userEmail)
+            } else {
+                LoginPasswordView(userEmail: userEmail, isSignIn: $isSignIn)
+            }
+        } label: {
+            Text("계속")
+                .modifier(GreenButtonModifier())
+        }
+    }
+    
+    // 카카오 로그인 버튼
+    var kakaoLoginButton: some View {
+        Button {
+            kakaoAuthStore.handleKakaoLogin()
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.gray)
+                .frame(width: UIScreen.screenWidth * 0.8, height: 44)
+                .overlay {
+                    Text("카카오로 로그인하기")
+                }
+        }
+    }
+    
+    // 구글 로그인 버튼
+    var googleLoginButton: some View {
+        Button {
+            authStore.googleSignIn()
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.gray)
+                .frame(width: UIScreen.screenWidth * 0.8, height: 44)
+                .overlay {
+                    Text("Google로 로그인하기")
+                }
+        }
+    }
+    
+    // 애플 로그인 버튼
+    var appleLoginButton: some View {
+        Button {
+            
+        } label: {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.black)
+                .frame(width: UIScreen.screenWidth * 0.8, height: 44)
+                .overlay {
+                    HStack {
+                        Image(systemName: "applelogo")
+                        Text("Apple로 로그인하기")
+                    }
+                    .foregroundColor(.white)
+                }
+        }
+    }
 }
-
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
@@ -107,16 +126,3 @@ struct LoginView_Previews: PreviewProvider {
             .environmentObject(AuthStore())
     }
 }
-
-////구글 로그인을 위한 rootViewController 정의
-//extension View {
-//    func getRootViewController() -> UIViewController {
-//        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-//            return .init()
-//        }
-//        guard let root = screen.windows.first?.rootViewController else {
-//            return .init()
-//        }
-//        return root
-//    }
-//}

c7ba69f79f308bad7d7d7df00d3409f418d1cbc6
mrgroomy <konakona0106@gmail.com>
2023-01-31 17:09:18 +0900

    [Feat] 다이어리 ViewModel Combine 적용 및 FirebaseService 추가 #33

--- BootCamping/BootCamping/ViewModels/DiaryStore.swift
+++ BootCamping/BootCamping/ViewModels/DiaryStore.swift
@@ -11,28 +11,19 @@ import FirebaseFirestore
 import Foundation
 import SwiftUI
 import FirebaseStorage
+import Combine
 
-/*
- struct Diary {
-     let id: String //글
-     let uid: String //유저
-     let diaryTitle: String //다이어리 제목
-     let diaryAddress: String //장소
-     let diaryContent: String //다이어리 내용
-     let diaryImageURL: [String] //사진
-     let diaryCreatedDate: Timestamp //작성날짜
-     let diaryVisitedDate: Date //방문날짜 (피커로..?)
-     let diaryLike: String //다이어리 좋아요
- }
- */
 class DiaryStore: ObservableObject {
     //저장된 다이어리 리스트
     @Published var diaryList: [Diary] = []
     //파베 기본 경로
     let database = Firestore.firestore()
     
+    //
+    private var cancellables = Set<AnyCancellable>()
+    
     //MARK: Create
-    func createDiary(diary: Diary, images: [Data]) {
+    func createDiary1(diary: Diary, images: [Data]) {
             Task {
                 do {
                     guard let userUID = Auth.auth().currentUser?.uid else { return }
@@ -146,20 +137,91 @@ class DiaryStore: ObservableObject {
             }
             
         }
-        removeImage(diaryToDelete)
         getData()
     }
-    
-    //MARK: Remove Storage
-    func removeImage(_ diary: Diary) {
-        let storageRef = Storage.storage().reference().child("DiaryImages")
-        for diaryImage in diary.diaryImageNames {
-            storageRef.child(diaryImage).delete { error in
-                if let error = error {
-                    print("Error removing image from storage: \(error.localizedDescription)")
+
+    //MARK: Read Diary Combine
+    func getDiarys() {
+        FirebaseService().getDiarysService()
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                    print("Failed get Diarys")
+                        return
+                case .finished:
+                    print("Finished get Diarys")
+                    return
                 }
+            } receiveValue: { [weak self] diarys in
+                self?.diaryList = diarys
             }
-        }
+            .store(in: &cancellables)
     }
- 
+    
+
+    //MARK: Create Diary Combine
+
+    func createDiary(diary: Diary, images: [Data]) {
+        FirebaseService().createDiaryService(diary: diary, images: images)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                    print("Failed Create Diary")
+                    return
+                case .finished:
+                    print("Finished Create Diary")
+                    return
+                }
+            } receiveValue: { _ in
+                
+            }
+            .store(in: &cancellables)
+    }
+    
+    //MARK: Update Diary Combine
+    func updateDiary(diary: Diary, images: [Data]) {
+        FirebaseService().createDiaryService(diary: diary, images: images)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                    print("Failed Update Diary")
+                    return
+                case .finished:
+                    print("Finished Update Diary")
+                    self.getDiarys()
+                    return
+                }
+            } receiveValue: { _ in
+                
+            }
+            .store(in: &cancellables)
+    }
+    
+    //MARK: Delete Diary Combine
+    func deletDiary(diary: Diary) {
+        FirebaseService().deleteDiaryService(diary: diary)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                    print("Failed Delete Diary")
+                    return
+                case .finished:
+                    self.getDiarys()
+                    print("Finished Delete Diary")
+                    return
+                }
+            } receiveValue: { _ in
+                
+            }
+            .store(in: &cancellables)
+
+    }
+    
 }

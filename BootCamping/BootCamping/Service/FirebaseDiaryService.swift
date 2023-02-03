3bf5290f3f957e96763ef40ee8fa5af47e558741
mxnkng <alsrud_9810@naver.com>
2023-02-03 16:20:43 +0900

    [Refactor] 파일 분리 #90

--- BootCamping/BootCamping/Service/FirebaseDiaryService.swift
+++ BootCamping/BootCamping/Service/FirebaseDiaryService.swift
@@ -31,24 +31,6 @@ enum FirebaseDiaryServiceError: Error {
     }
 }
 
-enum FirebaseBookmarkDiaryServiceError: Error {
-    case badSnapshot
-    case addBookmarkDiaryError
-    case removeBookmarkDiaryError
-    
-    var errorDescription: String? {
-        switch self {
-        case .badSnapshot:
-            return "북마크한 다이어리 가져오기 실패"
-        case .addBookmarkDiaryError:
-            return "다이어리 북마크 추가 실패"
-        case .removeBookmarkDiaryError:
-            return "다이어리 북마크 삭제 실패"
-        }
-    }
-}
-
-
 
 struct FirebaseDiaryService {
     
@@ -325,55 +307,6 @@ struct FirebaseDiaryService {
                         }
                     }
             }
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    //MARK: - Add BookmarkDiaryService
-    func addBookmarkDiaryService(diaryId: String) -> AnyPublisher<Void, Error> {
-        Future<Void, Error> { promise in
-            guard let userUID = Auth.auth().currentUser?.uid else { return }
-            
-            self.database
-                .collection("UserList")
-                .document(userUID)
-                .updateData([
-                "bookMarkedDiaries" : FieldValue.arrayUnion([diaryId])
-            ]) { error in
-                if let error = error {
-                    print(error)
-                    promise(.failure(FirebaseBookmarkDiaryServiceError.addBookmarkDiaryError))
-                } else {
-                    // TODO: - fetch,,
-                    /// 현재 로그인된 유저의 데이터를 가져오는 게 bookmark에서의 fetch,, !
-                    promise(.success(()))
-                }
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    //MARK: - Remove BookmarkDiaryService
-    func removeBookmarkDiaryService(diaryId: String) -> AnyPublisher<Void, Error> {
-        Future<Void, Error> { promise in
-            guard let userUID = Auth.auth().currentUser?.uid else { return }
-            
-            self.database
-                .collection("UserList")
-                .document(userUID)
-                .updateData([
-                "bookMarkedDiaries" : FieldValue.arrayRemove([diaryId])
-            ]) { error in
-                if let error = error {
-                    print(error)
-                    promise(.failure(FirebaseBookmarkDiaryServiceError.removeBookmarkDiaryError))
-                } else {
-                    // TODO: - fetch,,
-                    /// 현재 로그인된 유저의 데이터를 가져오는 게 bookmark에서의 fetch,, !
-                    promise(.success(()))
-                }
-            }
-            
         }
         .eraseToAnyPublisher()
     }

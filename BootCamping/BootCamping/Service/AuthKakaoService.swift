6aefb6d756c569a897c81c7e414d0c94300a38ac
mrgroomy <konakona0106@gmail.com>
2023-02-06 17:40:46 +0900

    [Feat] 카카오 로그인 서비스 Combine #56

--- BootCamping/BootCamping/Service/AuthKakaoService.swift
+++ BootCamping/BootCamping/Service/AuthKakaoService.swift
@@ -6,4 +6,69 @@
 //
 
 import Foundation
+import Combine
+import KakaoSDKAuth
+import KakaoSDKUser
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
 
+
+struct AuthKakaoService {
+    
+    // MARK: - 카카오 로그아웃
+    
+    func kakaoLogOutService() -> AnyPublisher<Void, Error> {
+        Future<Void, Error> { promise in
+            UserApi.shared.logout {(error) in
+                if let error = error {
+                    print("로그아웃 에러 : \(error)")
+                    promise(.failure(AuthServiceError.signOutError))
+                }
+                else {
+                    print("logout() success.")
+                    promise(.success(()))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    // MARK: - 카카오 로그인
+    
+    func kakaoLogInService() -> AnyPublisher<Void, Error> {
+        Future<Void, Error> { promise in
+            // 카카오 설치 되어있을때
+            if (UserApi.isKakaoTalkLoginAvailable()) {
+                
+                UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+                    if let error = error {
+                        print(error)
+                        promise(.failure(AuthServiceError.signInError))
+                    } else {
+                        print("kakaoLogin success")
+                        _ = oauthToken
+                        promise(.success(()))
+                    }
+                }
+            } else {
+                // 카카오 설치 안되어있을때
+                UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print("에러: \(error)")
+                        promise(.failure(AuthServiceError.signInError))
+                    }
+                    else {
+                        print("loginWithKakaoAccount() success.")
+                        _ = oauthToken
+                        promise(.success(()))
+
+                    }
+                    
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+}

3259d820e6844426dd7bf21cc1b78a5b5fa36681
erun94510 <erun9414@icloud.com>
2023-02-10 00:48:22 +0900

    [Fix] 캠핑장 지역검색 시 플레이스홀더 수정

--- BootCamping/BootCamping/Views/SearchCampingSpot/CampingSpotListSearchingView.swift
+++ BootCamping/BootCamping/Views/SearchCampingSpot/CampingSpotListSearchingView.swift
@@ -25,7 +25,7 @@ struct CampingSpotListSearchingView: View {
     
     var body: some View {
         VStack {
-            TextField("여행지를 검색해주세요.", text: $keywordForSearching)
+            TextField("캠핑하실 지역을 검색해주세요", text: $keywordForSearching)
                 .textFieldStyle(.roundedBorder)
                 .padding(.horizontal, UIScreen.screenWidth*0.03)
                 .onSubmit {

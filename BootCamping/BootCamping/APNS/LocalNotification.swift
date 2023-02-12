cb7d1c12b4ed1d932154311e79311472e38e986c
mxnkng <alsrud_9810@naver.com>
2023-02-13 02:27:58 +0900

    [Add] 특정 날짜에 대한 푸시 알림 구현 #104

--- BootCamping/BootCamping/APNS/LocalNotification.swift
+++ BootCamping/BootCamping/APNS/LocalNotification.swift
@@ -8,24 +8,6 @@
 import Foundation
 
 struct LocalNotification {
-    var identifier: String
     var title: String
     var body: String
-    var subtitle: String?
-    var dateComponents: DateComponents
-    var repeats: Bool
-    
-    init(identifier: String,
-         title: String,
-         body: String,
-         subtitle: String? = nil,
-         dateComponents: DateComponents,
-         repeats: Bool) {
-        self.identifier = identifier
-        self.title = title
-        self.body = body
-        self.subtitle = nil
-        self.dateComponents = dateComponents
-        self.repeats = repeats
-    }
 }

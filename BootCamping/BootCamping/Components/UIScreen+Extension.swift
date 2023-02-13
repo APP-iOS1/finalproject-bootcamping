5acf78447b6da65da2e72cc10e55cc5cade76e39
thekoon0456 <thekoon0456@gmail.com>
2023-02-13 20:58:13 +0900

    [Fix] imageScaleFix

--- BootCamping/BootCamping/Components/UIScreen+Extension.swift
+++ BootCamping/BootCamping/Components/UIScreen+Extension.swift
@@ -80,7 +80,7 @@ extension UIImage {
 
 extension Image {
     func resizeImage(imgName: String) -> Image {
-        guard let img = UIImage(named: imgName)?.resize(newWidth: UIScreen.screenWidth * 1 / 7) else { fatalError("Fail to load image") }
+        guard let img = UIImage(named: imgName)?.resize(newWidth: UIScreen.screenWidth) else { fatalError("Fail to load image") }
         return Image(uiImage: img)
     }
     func resizeImageData(data: Data) -> Image {

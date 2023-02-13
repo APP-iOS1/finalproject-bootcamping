6292e50becbade43762020cae8a6213a6677875c
erun94510 <erun9414@icloud.com>
2023-02-13 15:22:30 +0900

    [Add] 캠핑디테일뷰에 후기 더보기 버튼 활성화 및 뷰 구성

--- BootCamping/BootCamping/Views/SearchCampingSpot/DiaryListForCampingSpotView.swift
+++ BootCamping/BootCamping/Views/SearchCampingSpot/DiaryListForCampingSpotView.swift
@@ -1,36 +1,37 @@
-////
-////  DiaryListForCampingSpotView.swift
-////  BootCamping
-////
-////  Created by Donghoon Bae on 2023/02/10.
-////
 //
-//import SwiftUI
+//  DiaryListForCampingSpotView.swift
+//  BootCamping
 //
-//struct DiaryListForCampingSpotView: View {
-//    var diaryList: [Diary]
-//    
-//    var body: some View {
-//        VStack {
-//            ScrollView(showsIndicators: false) {
-//                LazyVStack {
-//                    ForEach(diaryList.indices, id: \.self) { index in
-//                        if diary.diaryIsPrivate == false {
-//                            DiaryCellView(item: diaryStore.userInfoDiaryList[index])
+//  Created by Donghoon Bae on 2023/02/10.
+//
+
+import SwiftUI
+
+struct DiaryListForCampingSpotView: View {
+    
+    var diaryList: [Diary]
+    
+    var body: some View {
+        VStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(diaryList.indices, id: \.self) { index in
+                        if diaryList[index].diaryIsPrivate == false {
+                            DiaryCellView(item: UserInfoDiary(diary: diaryList[index], user: User(id: "", profileImageName: "", profileImageURL: "", nickName: "", userEmail: "", bookMarkedDiaries: [], bookMarkedSpot: [], blockedUser: [])))
 //                                .task {
-//                                    if index == diaryStore.userInfoDiaryList.count - 1 {
+//                                    if index == diaryList.count - 1 {
 //                                        Task {
 //                                            diaryStore.nextGetRealtimeDiaryCombine()
 //                                        }
 //                                    }
-//                                }
-//                        }
-//                    }
-//                }
-//            }
-//    }
-//}
-//
+                                }
+                        }
+                    }
+                }
+            }
+    }
+}
+
 //struct DiaryListForCampingSpotView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        DiaryListForCampingSpotView()

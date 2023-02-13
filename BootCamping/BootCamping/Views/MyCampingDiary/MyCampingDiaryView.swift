f5392159d073c19aaa5f2b6d60a4118231dd0268
thekoon0456 <thekoon0456@gmail.com>
2023-02-13 20:49:32 +0900

    [Fix] merge

--- BootCamping/BootCamping/Views/MyCampingDiary/MyCampingDiaryView.swift
+++ BootCamping/BootCamping/Views/MyCampingDiary/MyCampingDiaryView.swift
@@ -26,34 +26,37 @@ struct MyCampingDiaryView: View {
     //faceId.isLocked // 페이스 아이디가 잠겨있는지.
     
     var body: some View {
-        NavigationStack{
         ZStack {
             VStack {
-                    ScrollView(showsIndicators: false) {
-                        LazyVStack {
-                            ForEach(diaryStore.myDiaryUserInfoDiaryList.indices, id: \.self) { index in
-                                DiaryCellView(item: diaryStore.myDiaryUserInfoDiaryList[index])
-                                    .task {
-                                        if index == diaryStore.myDiaryUserInfoDiaryList.count - 1 {
-                                            Task {
-                                                diaryStore.nextGetMyDiaryCombine()
-                                            }
+                ScrollView(showsIndicators: false) {
+                    LazyVStack {
+                        ForEach(diaryStore.myDiaryUserInfoDiaryList.indices, id: \.self) { index in
+                            DiaryCellView(item: diaryStore.myDiaryUserInfoDiaryList[index])
+                                .task {
+                                    if index == diaryStore.myDiaryUserInfoDiaryList.count - 1 {
+                                        Task {
+                                            diaryStore.nextGetMyDiaryCombine()
                                         }
+                                    }
                                     
                                 }
-                            }
                         }
-                        .refreshable {
-                            diaryStore.firstGetMyDiaryCombine()
-                        }
-                        .padding(.top)
-                        .padding(.bottom, 1)
                     }
-            .background(Color.bcWhite)
+                    .refreshable {
+                        diaryStore.firstGetMyDiaryCombine()
+                    }
+                    .padding(.top)
+                    .padding(.bottom, 1)
+                }
+                .background(Color.bcWhite)
+
+                
+            }
             //다이어리 비어있을때 추가 화면
             DiaryEmptyView().zIndex(-1)
             diaryStore.isProcessing ? Color.black.opacity(0.3) : Color.clear
 
+            
         }
         .toolbar{
             ToolbarItem(placement: .navigationBarLeading) {
@@ -61,19 +64,20 @@ struct MyCampingDiaryView: View {
                     .font(.title.bold())
             }
             ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink (destination: DiaryAddView())
-                    {
-                        Image(systemName: "plus")
-                    }
-            }
-             .alert("다이어리 만들기에 실패했습니다.. 다시 시도해 주세요.", isPresented: $diaryStore.isError) {
-                Button("확인", role: .cancel) {
-                    diaryStore.isError = false
+                NavigationLink (destination: DiaryAddView())
+                {
+                    Image(systemName: "plus")
                 }
-            .toast(isPresenting: $diaryStore.isProcessing) {
-                AlertToast(displayMode: .alert, type: .loading)
             }
+        }
+        .alert("다이어리 만들기에 실패했습니다.. 다시 시도해 주세요.", isPresented: $diaryStore.isError) {
+            Button("확인", role: .cancel) {
+                diaryStore.isError = false
+            }
+            
+        }
+        .toast(isPresenting: $diaryStore.isProcessing) {
+            AlertToast(displayMode: .alert, type: .loading)
         }
     }
 }
-

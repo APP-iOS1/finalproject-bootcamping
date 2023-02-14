ba28f16bd241a52abee53bbfb77dd9f5cc255349
mxnkng <alsrud_9810@naver.com>
2023-02-14 15:12:36 +0900

    [Add] 신고 되었음을 알려주는 뷰 추가 #194

--- BootCamping/BootCamping/Views/MyCampingDiary/Report/WaitingView.swift
+++ BootCamping/BootCamping/Views/MyCampingDiary/Report/WaitingView.swift
@@ -11,12 +11,15 @@ struct WaitingView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack(alignment: .center, spacing: 50){
+        VStack(alignment: .center, spacing: 20){
             Image(systemName: "exclamationmark.circle")
-                .frame(width: UIScreen.screenWidth*0.3, height: UIScreen.screenWidth*0.3)
+                .resizable()
+                .frame(width: UIScreen.screenWidth*0.2, height: UIScreen.screenWidth*0.2)
                 .foregroundColor(Color.accentColor)
             Text("익명의 사용자에게 이미 신고되어 관리자의 검토를 기다리는 게시물입니다.")
+                .multilineTextAlignment(.leading)
         }
+        .padding(.vertical, UIScreen.screenWidth*0.1)
     }
 }
 

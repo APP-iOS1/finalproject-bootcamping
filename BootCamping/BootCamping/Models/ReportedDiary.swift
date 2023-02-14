0ea34bfbf55a406b2c06497e8c497ed195ab247e
mxnkng <alsrud_9810@naver.com>
2023-02-14 14:11:00 +0900

    [Add] ReportedDiary 모델 추가 #194

---
+++ BootCamping/BootCamping/Models/ReportedDiary.swift
@@ -0,0 +1,14 @@
+//
+//  ReportedDiary.swift
+//  BootCamping
+//
+//  Created by 이민경 on 2023/02/14.
+//
+
+import Foundation
+
+struct ReportedDiary{
+    let id: String // UUID().uuidString
+    let reportedDiaryId: String // 신고된 다이어리 id
+    let reportOption: String // 신고 사유
+}

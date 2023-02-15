871b8ef3cbcf14d1122dece5682823d8b94b9c14
mxnkng <alsrud_9810@naver.com>
2023-02-15 16:57:42 +0900

    [Add] 차단한 사용자 관리하는 뷰 추가 #214

--- BootCamping/BootCamping/Views/Home/WeeklyPopulerCampingView.swift
+++ BootCamping/BootCamping/Views/Home/WeeklyPopulerCampingView.swift
@@ -14,13 +14,13 @@ struct WeeklyPopulerCampingView: View {
     @EnvironmentObject var diaryStore: DiaryStore
     @EnvironmentObject var commentStore: CommentStore
     @EnvironmentObject var diaryLikeStore: DiaryLikeStore
-    @EnvironmentObject var blockedUserStore: BlockedUserStore
+    @EnvironmentObject var wholeAuthStore: WholeAuthStore
     
     var body: some View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
-                    ForEach(diaryStore.popularDiaryList.filter{ !blockedUserStore.blockedUsers.contains($0.diary.uid) }, id: \.self) { item in
+                    ForEach(diaryStore.popularDiaryList.filter{ !wholeAuthStore.currnetUserInfo?.blockedUser.contains($0.diary.uid) }, id: \.self) { item in
                         NavigationLink {
                             DiaryDetailView(item: item) //맞나
                         } label: {

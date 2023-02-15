0368cd69c4a9abb979095fdec7f28414222244e9
mxnkng <alsrud_9810@naver.com>
2023-02-15 20:05:38 +0900

    [Fix] 차단하기 토스트 알림 수정 #214

--- BootCamping/BootCamping/Views/SearchCampingSpot/DiaryListForCampingSpotView.swift
+++ BootCamping/BootCamping/Views/SearchCampingSpot/DiaryListForCampingSpotView.swift
@@ -13,12 +13,17 @@ struct DiaryListForCampingSpotView: View {
     @StateObject var diaryStore: DiaryStore
     @StateObject var commentStore: CommentStore = CommentStore()
     
+    @EnvironmentObject var wholeAuthStore: WholeAuthStore
+    
+    @State private var isShowingAcceptedToast = false
+    @State private var isShowingBlockedToast = false
+    
     var body: some View {
         VStack {
             ScrollView(showsIndicators: false) {
                 LazyVStack {
-                    ForEach(diaryStore.realTimeDiaryUserInfoDiaryList, id: \.self) {  item in
-                            DiaryCellView(item: item)
+                    ForEach(diaryStore.realTimeDiaryUserInfoDiaryList.filter{ !wholeAuthStore.currnetUserInfo!.blockedUser.contains($0.diary.uid) }, id: \.self) { item in
+                        DiaryCellView(item: item, isShowingAcceptedToast: $isShowingAcceptedToast, isShowingBlockedToast: $isShowingBlockedToast)
                         }
                     }
             }

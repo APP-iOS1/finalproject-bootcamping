e15cce2eb7e6474553a37eb76ac27050f70c0bad
mxnkng <alsrud_9810@naver.com>
2023-02-15 13:40:57 +0900

    [Fix] 오타 수정 #194

--- BootCamping/BootCamping/Views/MyCampingDiary/DiaryDetailView.swift
+++ BootCamping/BootCamping/Views/MyCampingDiary/DiaryDetailView.swift
@@ -156,6 +156,9 @@ struct DiaryDetailView: View {
             .toast(isPresenting: $isShowingAcceptedToast) {
                 AlertToast(type: .regular, title: "이 게시물에 대한 신고가 접수되었습니다.")
             }
+            .toast(isPresenting: $isShowingBlockedToast) {
+                AlertToast(type: .regular, title: "이 사용자를 차단했습니다.", subTitle: "차단 해제는 마이페이지 > 설정에서 가능합니다.")
+            }
             .sheet(isPresented: $isShowingUserReportAlert) {
                 if reportState == .alreadyReported {
                     WaitingView()

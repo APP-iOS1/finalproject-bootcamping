4b9727961318603e00f6dfbd58d15ec27445600b
thekoon0456 <thekoon0456@gmail.com>
2023-03-26 18:57:18 +0900

    [Fix] 캠핑장아이콘 누락된것 다시 추가

--- BootCamping/BootCamping/Views/MyCampingDiary/DiaryCellView.swift
+++ BootCamping/BootCamping/Views/MyCampingDiary/DiaryCellView.swift
@@ -77,7 +77,6 @@ struct DiaryCellView: View {
                 .background(Color.secondary)
             } else {
                 diaryImage
-                    .edgesIgnoringSafeArea(.all)
                 
                 NavigationLink {
                     DiaryDetailView(item: item)
